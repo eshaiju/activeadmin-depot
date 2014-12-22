5714d11c7d7ff6d118366d62c6494bec8c0949b5
Shaiju E <eshaiju@gmail.com>
2014-12-22 10:47:25 +0530

    make it better

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ActiveRecord::Base
+
+  # Named Scopes
+  scope :available, lambda{ where("available_on < ?", Date.today) }
+  scope :drafts, lambda{ where("available_on > ?", Date.today) }
+
+  # Validations
+  validates_presence_of :title
+  validates_presence_of :price
+  validates_presence_of :image_file_name
+
+end
